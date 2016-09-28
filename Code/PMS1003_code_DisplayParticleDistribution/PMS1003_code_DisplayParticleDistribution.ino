804a90a96cf758680ede16e5352175378d1e29fa
Jamie Van de Laar <jpv15@uclive.ac.nz>
2016-09-28 15:40:44 +1300

    Tidying/commenting PMS1003 code for hand over

--- Code/PMS1003_code_DisplayParticleDistribution/PMS1003_code_DisplayParticleDistribution.ino
+++ Code/PMS1003_code_DisplayParticleDistribution/PMS1003_code_DisplayParticleDistribution.ino
@@ -1,7 +1,6 @@
 //****************************************************************************************************************
-//*Abstract: Code used to log total particle concentration of particles with assumed DENSITY and MAX_PARTICLE_SIZE
-//           using PLX-DAQ software for Excel. Also contains code for using GoBetwino software. This was run using
-//           an Arduino Nano.
+//*Abstract: Code used to display incremental particle concentrations (with percentages) and total concentration 
+//           of particles with assumed DENSITY and MAX_PARTICLE_SIZE. This was run using an Arduino Nano.
 //
 //*Author：Jamie Van de Laar
 //*Modified from program originally written by Zuyang @ HUST
@@ -16,7 +15,7 @@
 /*********************** Define Constants ************************/ 
 
 #define LENG 31               // 0x42 + 31 bytes equal to 32 bytes
-#define DENSITY 1.31e-6       // Bulk density of particulates in ug/um^3, PMS1003 assumes approx 3.67e-6 for PM readings
+#define DENSITY 3.67e-6       // Bulk density of particulates in ug/um^3, PMS1003 assumes approx 3.67e-6 for PM readings
 #define MAX_PARTICLE_SIZE 40  // Max expected particle size in microns - Using 40 for AC Fine Test Dust and 
                               // ISO 12103-1, A2 Fine Test Dust
 #define DP 2                  // Decimal places for display of floats
@@ -39,6 +38,11 @@ unsigned long numParticles_over_2_5 = 0;
 unsigned long numParticles_over_5 = 0;
 unsigned long numParticles_over_10 = 0;
 
+// Calculated PM values to compare to sensor given values
+float myPM01conc = 0;
+float myPM2_5conc = 0;
+float myPM10conc = 0;
+
 // Variables for concentrations in ug/m^3
 float PM005conc = 0;      // Conc between 0.3 and 0.5
 float PM01conc = 0;       // Conc between 0.5 and 1.0
@@ -51,14 +55,11 @@ float totalConc_prev = 0; // Previous total concentration, stored so it is known
 
 
 /*********************** Setup Code ************************/
- 
+
 void setup()
 {
   Serial.begin(9600);   //use serial0
   Serial.setTimeout(1500);    //set the Timeout to 1500ms, longer than the data transmission periodic time of the sensor
-
-  // Create label - only for PLX-DAQ!!
-  Serial.println("LABEL,Time of day,Time Since Start,Conc");
 }
 
 
@@ -86,6 +87,11 @@ void loop()
         numParticles_over_5 = returnData(buf, 23);
         numParticles_over_10 = returnData(buf, 25);
 
+        // Calculate PM1/PM2.5/PM10 values to compare to sensor given values, uncomment to use
+//        myPM01conc = return_ugConc(numParticles_over_0_3 - numParticles_over_1, (0.3 + 1)/2/2);
+//        myPM2_5conc = return_ugConc(numParticles_over_1 - numParticles_over_2_5, (1 + 2.5)/2/2);
+//        myPM10conc = return_ugConc(numParticles_over_2_5 - numParticles_over_10, (2.5 + 10)/2/2);
+
         // Calculate particle concentration in each size bin
         PM005conc = return_ugConc(numParticles_over_0_3 - numParticles_over_0_5, (0.3 + 0.5)/2/2);
         PM01conc = return_ugConc(numParticles_over_0_5 - numParticles_over_1, (0.5 + 1)/2/2);
@@ -94,31 +100,94 @@ void loop()
         PM10conc = return_ugConc(numParticles_over_5 - numParticles_over_10, (5 + 10)/2/2);
         PM_Over10conc = return_ugConc(numParticles_over_10, (10 + MAX_PARTICLE_SIZE)/2/2);
 
-        // Sum to get total concentration in ug/m^3, PM005 may wish to be commented out due to occasionally 
-        // incorrect readings
+        // Sum to get total concentration in ug/m^3
         totalConc = PM005conc + PM01conc + PM2_5conc + PM5conc + PM10conc + PM_Over10conc;
       }
-    }
+    } 
   }
  
-
-  if (totalConc != totalConc_prev)  // If the sensor has updated the data
+  if (totalConc != totalConc_prev)  // If the sensor has updated the data, display the new data
   {
-    // GoBetwino Logging, uncomment to use GoBetwino (comment out PLX-DAQ code)
-//    Serial.print("#S|PMSLOG|[");
-//    Serial.print(float(millis())/1000, DP);
-//    Serial.print(",");
-//    Serial.print(totalConc, DP);
-//    Serial.println("]#");
+    // Uncomment following code to compare calculated PM concentration to sensor given values
+    
+//    Serial.print("PM1.0:             ");  
+//    Serial.print(PM01Value);
+//    Serial.println("  ug/m3");            
+//   
+//    Serial.print("PM2.5:             ");  
+//    Serial.print(PM2_5Value);
+//    Serial.println("  ug/m3");     
+//     
+//    Serial.print("PM10:              ");  
+//    Serial.print(PM10Value);
+//    Serial.println("  ug/m3");   
+//
+//    Serial.print("My PM01 conc:      ");  
+//    Serial.print(myPM01conc, DP);
+//    Serial.println("  ug/m3");
+//
+//    Serial.print("My PM2.5 conc:     ");  
+//    Serial.print(myPM2_5conc, DP);
+//    Serial.println("  ug/m3");
+//
+//    Serial.print("My PM10 conc:      ");  
+//    Serial.print(myPM10conc, DP);
+//    Serial.println("  ug/m3");
+//    Serial.println();
 
-    // PLX-DAQ Logging
-    Serial.print("DATA,TIME,");
-    Serial.print(float(millis())/1000, DP);
-    Serial.print(",");
-    Serial.println(totalConc, DP);
+
+    Serial.print("Total conc:             ");
+    Serial.print(totalConc, DP);   // In ug/m3
+    Serial.println("  ug/m3"); 
+//    Serial.print(totalConc/1000, DP);   // In mg/m3
+//    Serial.println("  mg/m3");
+
+    Serial.print("0.03 to 0.05 um conc:   ");  
+    Serial.print(PM005conc, DP);
+    Serial.print("  ug/m3,   ");
+    Serial.print((PM005conc/totalConc)*100, DP);
+    Serial.println("%");
+
+    Serial.print("0.05 to 0.1 um conc:    ");  
+    Serial.print(PM01conc, DP);
+    Serial.print("  ug/m3,   ");  
+    Serial.print((PM01conc/totalConc)*100, DP);
+    Serial.println("%");
+
+    Serial.print("0.1 to 2.5 um conc:     ");  
+    Serial.print(PM2_5conc, DP);
+    Serial.print("  ug/m3,   ");  
+    Serial.print((PM2_5conc/totalConc)*100, DP);
+    Serial.println("%");
+
+    Serial.print("2.5 to 5 um conc:       ");  
+    Serial.print(PM5conc, DP);
+    Serial.print("  ug/m3,   ");  
+    Serial.print((PM5conc/totalConc)*100, DP);
+    Serial.println("%");
+
+    Serial.print("5 to 10 um conc:        ");  
+    Serial.print(PM10conc, DP);
+    Serial.print("  ug/m3,   ");  
+    Serial.print((PM10conc/totalConc)*100, DP);
+    Serial.println("%");  
+
+    Serial.print(">10 um conc:            ");  
+    Serial.print(PM_Over10conc, DP);
+    Serial.print("  ug/m3,   ");  
+    Serial.print((PM_Over10conc/totalConc)*100, DP);
+    Serial.println("%");  
+
+    Serial.print("Over 10.0 in 0.1L: ");  
+    Serial.print(numParticles_over_10);
+    Serial.println("  particles");
+    
+    Serial.println();
+    Serial.println();
 
     totalConc_prev = totalConc;
   }
+   
 }
 
 
--- Code/PMS1003_code_SerialLog_uncut/PMS1003_code_SerialLog_uncut.ino
+++ Code/PMS1003_code_SerialLog_uncut/PMS1003_code_SerialLog_uncut.ino
@@ -1,24 +1,22 @@
-//****************************************************************************************************************
-//*Abstract: Code used to log total particle concentration of particles with assumed DENSITY and MAX_PARTICLE_SIZE
-//           using PLX-DAQ software for Excel. Also contains code for using GoBetwino software. This was run using
-//           an Arduino Nano.
-//
-//*Author：Jamie Van de Laar
-//*Modified from program originally written by Zuyang @ HUST
-//*Last modified：September.28.2016
-//
-//*Notes: -Green wire is TX on Arduino
-//        -"Sleep" pin must be grounded in order to upload code to Arduino while PMS1003 is connected
-//****************************************************************************************************************
+//******************************
+ //*Abstract: Code used to log total particle concentration of matter with assumed DENSITY and MAX_PARTICLE_SIZE
+ //           using PLX-DAQ software for Excel. Also contains code for using GoBetwino software. This was run using
+ //           an Arduino Nano.
+ //
+ //*Author：Jamie Van de Laar
+ //*Modified from program originally written by Zuyang @ HUST
+ //*Date：August.16.2016
+ //
+ //*Notes: Green wire is TX on Arduino
+ //******************************
 
 #include <Arduino.h>
 
 /*********************** Define Constants ************************/ 
 
-#define LENG 31               // 0x42 + 31 bytes equal to 32 bytes
-#define DENSITY 1.31e-6       // Bulk density of particulates in ug/um^3, PMS1003 assumes approx 3.67e-6 for PM readings
-#define MAX_PARTICLE_SIZE 40  // Max expected particle size in microns - Using 40 for AC Fine Test Dust and 
-                              // ISO 12103-1, A2 Fine Test Dust
+#define LENG 31               //0x42 + 31 bytes equal to 32 bytes
+#define DENSITY 1.31e-6       // Density of particulates in ug/um^3, PMS1003 assumes ~3.67 for PM readings
+#define MAX_PARTICLE_SIZE 40  // Max expected particle size in microns - USING 40 FOR AC FINE DUST AND ISO DUST!!
 #define DP 2                  // Decimal places for display of floats
 
 
@@ -47,7 +45,10 @@ float PM5conc = 0;        // Conc between 2.5 and 5.0
 float PM10conc = 0;       // Conc between 5.0 and 10.0
 float PM_Over10conc = 0;  // Conc between 10.0 and MAX_PARTICLE_SIZE
 float totalConc = 0;      // Total concentration in ug/m^3
-float totalConc_prev = 0; // Previous total concentration, stored so it is known when the sensor has new data
+float totalConc_prev = 0;
+
+// This variable can be used to determine the sample period between successive reads off the sensor
+float sampleTime = 0;
 
 
 /*********************** Setup Code ************************/
@@ -57,7 +58,7 @@ void setup()
   Serial.begin(9600);   //use serial0
   Serial.setTimeout(1500);    //set the Timeout to 1500ms, longer than the data transmission periodic time of the sensor
 
-  // Create label - only for PLX-DAQ!!
+  // Create label - only for PLX-DAQ!!!
   Serial.println("LABEL,Time of day,Time Since Start,Conc");
 }
 
@@ -69,7 +70,7 @@ void loop()
   // Start to read from sensor when detect 0x42
   if(Serial.find(0x42)){
     
-    Serial.readBytes(buf, LENG);
+    Serial.readBytes(buf,LENG);
     if(buf[0] == 0x4d){
       if(checkValue(buf, LENG)){
         
@@ -94,37 +95,43 @@ void loop()
         PM10conc = return_ugConc(numParticles_over_5 - numParticles_over_10, (5 + 10)/2/2);
         PM_Over10conc = return_ugConc(numParticles_over_10, (10 + MAX_PARTICLE_SIZE)/2/2);
 
-        // Sum to get total concentration in ug/m^3, PM005 may wish to be commented out due to occasionally 
-        // incorrect readings
-        totalConc = PM005conc + PM01conc + PM2_5conc + PM5conc + PM10conc + PM_Over10conc;
+        // Sum to get total concentration in ug/m^3, PM005 commented out due to faulty readings
+        totalConc = /*PM005conc +*/ PM01conc + PM2_5conc + PM5conc + PM10conc + PM_Over10conc;
       }
     }
   }
  
-
-  if (totalConc != totalConc_prev)  // If the sensor has updated the data
+  static unsigned long OledTimer=millis();  
+  if (millis() - OledTimer >= 1000) 
+//    if (totalConc != totalConc_prev)
   {
-    // GoBetwino Logging, uncomment to use GoBetwino (comment out PLX-DAQ code)
-//    Serial.print("#S|PMSLOG|[");
-//    Serial.print(float(millis())/1000, DP);
-//    Serial.print(",");
-//    Serial.print(totalConc, DP);
-//    Serial.println("]#");
+    sampleTime = float(millis() - OledTimer)/1000;
+    OledTimer = millis();
 
-    // PLX-DAQ Logging
+    // GoBetwino Suff, uncomment to use GoBetwino (comment out PLX-DAQ code)
+//      Serial.print("#S|PMSLOG|[");
+//      Serial.print(float(OledTimer)/1000, DP);
+//      Serial.print(",");
+//      Serial.print(totalConc, DP);
+//      Serial.println("]#");
+
+    // PLX-DAQ Stuff
     Serial.print("DATA,TIME,");
-    Serial.print(float(millis())/1000, DP);
+    Serial.print(float(OledTimer)/1000, DP);
+    Serial.print(",");
+    Serial.print(sampleTime, DP);
     Serial.print(",");
     Serial.println(totalConc, DP);
 
     totalConc_prev = totalConc;
+
   }
+   
 }
 
 
 /*********************** Define Functions ************************/
 
-// Check the data from the sensor is valid
 char checkValue(unsigned char *thebuf, char leng)
 {  
   char receiveflag=0;
@@ -143,15 +150,12 @@ char checkValue(unsigned char *thebuf, char leng)
   return receiveflag;
 }
 
-
-// Returns 16 bit data from the specified starting index in the buffer (index of the first byte)
 unsigned long returnData(unsigned char *thebuf, int index1)
 {
   unsigned long particleVal;
   particleVal = ((thebuf[index1]<<8) + thebuf[index1 + 1]); //count specified particles
   return particleVal;
 }
-
 
 // Return concentration in ug/m^3 for given number of particles with given average radius in microns
 float return_ugConc(unsigned long numParticles, float avgRadius)
@@ -160,5 +164,4 @@ float return_ugConc(unsigned long numParticles, float avgRadius)
   C = DENSITY*10000*float(numParticles)*(4/3)*M_PI*avgRadius*avgRadius*avgRadius;
   return C;
 }
-
 
